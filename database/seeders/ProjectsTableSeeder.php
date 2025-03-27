91a80f79dccaec901cdb0b21d999e877802a6f5b
federicascognamiglio <scognamigliofede99@gmail.com>
2025-03-27 16:04:53 +0100

    feat: projects seeder created

---
+++ database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Faker\Generator as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+           $newProject = new Project();
+
+            $newProject->nome = $faker->sentence(3);
+            $newProject->cliente = $faker->company();
+            $newProject->data_inizio = $faker->date();
+            $newProject->data_fine = $faker->date();
+            $newProject->riassunto = $faker->paragraph(4);
+
+            $newProject->save();
+        }
+    }
+}
