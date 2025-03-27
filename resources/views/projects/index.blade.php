4abc6901cc0f34a4e49150023decc4a5a8df9060
federicascognamiglio <scognamigliofede99@gmail.com>
2025-03-27 16:06:28 +0100

    feat: portfolio index created

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,11 @@
+@extends ('layouts.master')
+
+<!-- @section ('title', "MyProjects") -->
+@section ('content')
+<div class="container">
+    <h1 class="mt-5 mb-3 text-center">My Projects</h1>
+    <ul>
+        
+    </ul>
+</div>
+@endsection
