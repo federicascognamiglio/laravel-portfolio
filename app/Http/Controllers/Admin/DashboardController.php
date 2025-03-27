ba98223345da0c367f3bee1a9f147f1ead0b1b38
federicascognamiglio <scognamigliofede99@gmail.com>
2025-03-27 14:13:30 +0100

    feat: dashboard controller added

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        return "INDEX";
+    }
+
+    public function profile()
+    {
+        return "PROFILE";
+    }
 }
