668ed6b81be500d8c6b7b392f282e433f177f9a2
federicascognamiglio <scognamigliofede99@gmail.com>
2025-04-07 12:03:49 +0200

    feat: aggiungo le relazioni tra i models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Type;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Project;
 use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
-    //
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 }
