101d3a468dc2f463c78a3903ba34c64c2ab92a44
federicascognamiglio <scognamigliofede99@gmail.com>
2025-04-07 14:41:41 +0200

    feat: technologies model created and connected to projects

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,8 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+    // Definisco la tabella types associata al modello
     public function type()
     {
         return $this->belongsTo(Type::class);
+    }
+
+    // Definisco la tabella technologies associata al modello
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class);
     }
 }
