23badce4efeb5de2e4b37b1e99da155c935eb6b4
federicascognamiglio <scognamigliofede99@gmail.com>
2025-04-09 12:17:52 +0200

    feat: show api route created

--- app/Http/Controllers/Api/ProjectsController.php
+++ app/Http/Controllers/Api/ProjectsController.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Project;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
-    public function index () {
-        return 'Sei nella index';
+    public function index() {
+
+        // Prelevo i progetti dal database con collegamento types
+        $projects = Project::with('type')->get();
+
+        // Ritorno i dati in formato JSON
+        return response()->json([
+            'success' => true,
+            'data' => $projects
+        ]);
+    }
+
+    public function show(Project $project) {
+        // Prelevo il progetto dal database con collegamenti
+        $project->load('type', 'technologies');
+
+        // Ritorno i dati in formato JSON
+        return response()->json([
+            'success' => true,
+            'data' => $project
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// Index
 Route::get('/projects', [ProjectsController::class, 'index']);
+
+// Show
+Route::get('/projects/{project}', [ProjectsController::class, 'show']);
