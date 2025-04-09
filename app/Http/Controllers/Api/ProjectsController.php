454577806bccbebb1446296cbde9c064c660f841
federicascognamiglio <scognamigliofede99@gmail.com>
2025-04-09 11:23:36 +0200

    feat: api projects controller created

---
+++ app/Http/Controllers/Api/ProjectsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function index () {
+        return 'Sei nella index';
+    }
+}
